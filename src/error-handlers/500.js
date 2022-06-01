93f73ed959f7fc03bae5133e4d2b9bf29a6d66da
Bahaa Nimer <90227044+BahaaNimer@users.noreply.github.com>
2022-06-01 20:19:40 +0300

    Update 500.js

--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -4,13 +4,13 @@ module.exports = (error, req, res, next) => {
     res.status(500).send({
       code: 500,
       route: req.path,
-      message: `Server Error:${error.message}`,
+      message: `Server Error:${error}`,
     })
   } else if (req.query.name === 'wrong Name') {
     res.status(500).send({
       code: 500,
       route: req.path,
-      message: `Server Error:${error.message}`,
+      message: `Server Error:${error}`,
     })
   };
 };
